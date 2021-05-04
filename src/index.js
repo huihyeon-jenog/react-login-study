bb7a53df73be352bbe810d6256b6984effae3696
huihyeon jeong <wjdgmlgus5@naver.com>
2021-05-04 18:07:22 +0900

    ...

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
+import { AppContainer } from "react-hot-loader";
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import Root from "./Root";
+import "./index.css";
 import configureStore from "./redux/configureStore";
 
 const store = configureStore();
+const render = (Component) => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component store={store} />
+    </AppContainer>,
+    document.getElementById("root")
+  );
+};
 
-ReactDOM.render(<Root store={store} />, document.getElementById("root"));
+render(Root);
+
+if (module.hot) {
+  module.hot.accept("./Root", () => render(Root));
+}
--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,9 +1,27 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as baseActions from "..//redux/modules/base";
 
-export class Auth extends Component {
+class Auth extends Component {
+  // 페이지에 진입 할 때 헤더를 비활성화
+  componentWillMount() {
+    this.props.BaseActions.setHeaderVisibility(false);
+  }
+
+  // 페이지에서 벗어 날 때 다시 활성화
+  componentWillUnmount() {
+    this.props.BaseActions.setHeaderVisibility(true);
+  }
+
   render() {
     return <div>Auth</div>;
   }
 }
 
-export default Auth;
+export default connect(
+  (state) => ({}),
+  (dispatch) => ({
+    BaseActions: bindActionCreators(baseActions, dispatch),
+  })
+)(Auth);
